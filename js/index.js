f778afde54ed0c6f19bd68af9a192cfa3f7dc537
gentsao <61525709+gentsaot@users.noreply.github.com>
2020-08-27 19:32:34 -0400

    Layout tweaks + JS animation
    Added hover animation in hero section. Removed margin + padding from page. Tweaked first row across each grid to have consistent spacing.

---
+++ js/index.js
@@ -0,0 +1,28 @@
+console.log("Your index.js has loaded correctly! ^_^");
+
+$(document).ready(function(){
+    $("#orchids").hover(function(){
+        $("#orchid-reveal").css("opacity","100%");
+    },
+    function(){
+        $("#orchid-reveal").css("opacity","0%")
+    });
+});
+
+$(document).ready(function(){
+    $("#paint").hover(function(){
+        $("#paint-reveal").css("opacity","100%");
+    },
+    function(){
+        $("#paint-reveal").css("opacity","0%")
+    });
+});
+
+$(document).ready(function(){
+    $("#cat").hover(function(){
+        $("#cat-reveal").css("opacity","100%");
+    },
+    function(){
+        $("#cat-reveal").css("opacity","0%")
+    });
+});
